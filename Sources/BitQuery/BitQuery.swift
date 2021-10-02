bea7a116d51166e806222312d345e65594c2ab6b
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2021-10-01 22:35:23 -0400

    Implement getMask function.

--- Sources/BitQuery/BitQuery.swift
+++ Sources/BitQuery/BitQuery.swift
@@ -1,6 +1,18 @@
-public struct BitQuery {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+let bitMasks: [UInt8] = [
+     0b00000000,
+     0b00000001,
+     0b00000011,
+     0b00000111,
+     0b00001111,
+     0b00011111,
+     0b00111111,
+     0b01111111,
+     0b11111111
+ ]
+
+public func getMask(bitStart: Int, len: Int) -> UInt8 {
+    return (bitMasks[len] << bitStart)
+}
 }
---
+++ Tests/BitQueryTests/GetMaskTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import BitQuery
+
+final class GetMaskTests: XCTestCase {
+    let data = Data([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x55, 0xFF])
+
+    func testGetMask() { 
+        XCTAssertEqual(getMask(bitStart: 0, len: 8), 0b11111111)
+        XCTAssertEqual(getMask(bitStart: 0, len: 8), 0b11111111)
+        XCTAssertEqual(getMask(bitStart: 2, len: 3), 0b00011100)
+        XCTAssertEqual(getMask(bitStart: 7, len: 1), 0b10000000)
+    }
+}
